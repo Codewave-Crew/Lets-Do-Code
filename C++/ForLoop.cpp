758cbcd86b45634258821c7f45586c952d5b47d5
ErangaSenavirathne <erangasenevirathna@gmail.com>
2022-10-16 10:49:19 +0530

    Hello World in a Loop Using C++

---
+++ C++/ForLoop.cpp
@@ -0,0 +1,12 @@
+// C++ Program to display "Hello World!" 5 times
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        cout <<  "Hello World! " << endl;
+    }
+    return 0;
+}
