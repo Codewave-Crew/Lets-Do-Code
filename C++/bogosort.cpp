9c5ba88c78b7e65f565771347a6f03d512127135
kashyap-ji <97298851+kashyap-ji@users.noreply.github.com>
2022-10-16 20:49:09 +0530

    Create bogosort.cpp
    Bogosort program in C++

---
+++ C++/bogosort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isSorted(int a[], int n)
+{
+	while (--n > 0)
+		if (a[n] < a[n - 1])
+			return false;
+	return true;
+}
+void shuffle(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		swap(a[i], a[rand() % n]);
+}
+void bogosort(int a[], int n)
+{
+	while (!isSorted(a, n))
+		shuffle(a, n);
+}
+void printArray(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+int main()
+{
+  int n;
+	cout<<"Enter number of elements to be entered in the array:"<<endl;
+  cin>>n;
+  arr=new int[n];
+  cout<<"Enter "<<n<<" elements to be sorted:"<<endl;
+	bogosort(a, n);
+	printf("Sorted array :\n");
+	printArray(a, n);
+	return 0;
+}
