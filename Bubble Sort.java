371c4545020d2f50a18aef3db98c5d19d4de484a
ADRITA DEY <91267306+ADRITATITIR@users.noreply.github.com>
2022-10-17 00:28:03 +0530

    Added code for bubble sort in java

---
+++ Bubble Sort.java
@@ -0,0 +1,35 @@
+
+class BubbleSort {
+	void bubbleSort(int nums[])
+	{
+		int n = nums.length;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (nums[j] > nums[j + 1]) {
+					// swap nums[j+1] and nums[j]
+					int temp = nums[j];
+					nums[j] = nums[j + 1];
+					nums[j + 1] = temp;
+				}
+	}
+
+	/* Prints the array */
+	void printnumber(int nums[])
+	{
+		int n = nums.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(nums[i] + " ");
+		System.out.println();
+	}
+
+	// Driver method to test above
+	public static void main(String args[])
+	{
+		BubbleSort ob = new BubbleSort();
+		int nums[] = { 64, 34, 25, 12, 22, 11, 90 };
+		ob.bubbleSort(arr);
+		System.out.println("Sorted nums");
+		ob.printnumber(nums);
+	}
+}
+
