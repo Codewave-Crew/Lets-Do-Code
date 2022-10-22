771e45a406634e0b392a2a86ef20ca7c7e85070c
SongbidDey <112577099+SongbidDey@users.noreply.github.com>
2022-10-22 14:21:22 +0530

    Create Simple_Calculator.cpp
    Make a simple calculator using C++ programming language

---
+++ C++/Simple_Calculator.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+  char op;
+  float num1, num2;
+
+  cout << "Enter operator: +, -, *, /: ";
+  cin >> op;
+
+  cout << "Enter two operands: ";
+  cin >> num1 >> num2;
+
+  switch(op) {
+
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+
+    default:
+      // If the operator is other than +, -, * or /, error message is shown
+      cout << "Error! operator is not correct";
+      break;
+  }
+
+  return 0;
+}
+
+/*
+
+Enter operator: +, -, *, /: -    
+Enter two operands: 3.4 8.4
+3.4 - 8.4 = -5
+
+*/
