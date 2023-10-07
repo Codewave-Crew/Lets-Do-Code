b55baee7592e58f337d597b806de85fb3c95b0a1
Ayush Gawande <107213166+Ayu5-h@users.noreply.github.com>
2023-10-08 00:41:06 +0530

    Update MergeSort.java
    Merge sort but here the array is taken from the user rather than putting it in the code itself.

--- Java/MergeSort/MergeSort.java
+++ Java/MergeSort/MergeSort.java
@@ -1,77 +1,96 @@
-public class MergeSort {
+import java.util.*;
+public class merge {
 
-  void merge(int arr[], int l, int m, int r)
+	void mergesort(int arr[], int l, int m, int r)
+
 	{
-		int n1 = m - l + 1;
-		int n2 = r - m;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	int L[] = new int[n1];
+	int R[] = new int[n2];
 
-		int L[] = new int[n1];
-		int R[] = new int[n2];
-
-		for (int i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
-		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
-
-		int i = 0, j = 0;
-
-		int k = l;
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
-			}
-			else {
-				arr[k] = R[j];
-				j++;
-			}
-			k++;
-		}
-
-		while (i < n1) {
-			arr[k] = L[i];
-			i++;
-			k++;
-		}
-
-		while (j < n2) {
-			arr[k] = R[j];
-			j++;
-			k++;
-		}
+	for (int i = 0; i < n1; ++i) {
+		L[i] = arr[l + i];
 	}
 
+	for (int j = 0; j < n2; ++j) {
+		R[j] = arr[m + 1 + j];
+	}	
+	int i = 0, j = 0;
+	int k = l;
+
+	while (i < n1 && j < n2) {
+		
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+			}
+		else {
+			arr[k] = R[j];
+			j++;
+			}
+		k++;
+
+	}
+	while (i < n1) {
+		
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+		}
+
+	}
 	void sort(int arr[], int l, int r)
+
 	{
 		if (l < r) {
 			int m = l + (r - l) / 2;
-
 			sort(arr, l, m);
 			sort(arr, m + 1, r);
+			mergesort(arr, l, m, r);
+			}
 
-			merge(arr, l, m, r);
-		}
 	}
 
 	static void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
 
+	{
+
+	int n = arr.length;
+	for (int i = 0; i < n; ++i) {
+		System.out.print(arr[i] + " ");
+	}
+	
+	System.out.println();
+
+	}
 	public static void main(String args[])
 	{
-		int arr[] = { 5, 3, 9, 8, 1 };
+	      int i, n, x, arr[];
 
-		System.out.println("Given Array");
-		printArray(arr);
+	      Scanner s = new Scanner(System.in);
+	      System.out.println("Enter no. of elements in aray:");
+	      n = s.nextInt(); 
 
-		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.length - 1);
+	      arr = new int[n];
 
-		System.out.println("\nSorted array");
-		printArray(arr);
+	      System.out.println("Enter " + n + " integers:");
+
+	      for (i = 0;i<n; i++)
+	          arr[i] = s.nextInt();
+	      
+	      merge ob = new merge();
+	      ob.sort(arr, 0, arr.length - 1);
+	      System.out.println("\nSorted array is");
+
+	printArray(arr);
 	}
+
 }
+
+
