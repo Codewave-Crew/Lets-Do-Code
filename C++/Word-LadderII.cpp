e197e8f2aadd1e8f5a2f0402386ce8b934abf003
Aditya Srivastav <137613253+AdityaSrivastav5@users.noreply.github.com>
2023-10-10 19:39:34 +0530

    Word-LadderII.cpp

---
+++ C++/Word-LadderII.cpp
@@ -0,0 +1,135 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+vector<vector<string>> findSequences(string beginWord, string endWord,
+                                         vector<string> &wordList)
+    {
+        // Push all values of wordList into a set
+        // to make deletion from it easier and in less time complexity.
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        
+        // Creating a queue ds which stores the words in a sequence which is
+        // required to reach the targetWord after successive transformations.
+        queue<vector<string>> q;
+
+        // BFS traversal with pushing the new formed sequence in queue 
+        // when after a transformation, a word is found in wordList.
+
+        q.push({beginWord});
+
+        // A vector defined to store the words being currently used
+        // on a level during BFS.
+        vector<string> usedOnLevel;
+        usedOnLevel.push_back(beginWord);
+        int level = 0;
+       
+        // A vector to store the resultant transformation sequence.
+        vector<vector<string>> ans;
+        while (!q.empty())
+        {
+            vector<string> vec = q.front();
+            q.pop();
+
+            // Now, erase all words that have been
+            // used in the previous levels to transform
+            if (vec.size() > level)
+            {
+                level++;
+                for (auto it : usedOnLevel)
+                {
+                    st.erase(it);
+                }
+            }
+
+            string word = vec.back();
+
+            // store the answers if the end word matches with targetWord.
+            if (word == endWord)
+            {
+                // the first sequence where we reached end
+                if (ans.size() == 0)
+                {
+                    ans.push_back(vec);
+                }
+                else if (ans[0].size() == vec.size())
+                {
+                    ans.push_back(vec);
+                }
+            }
+            for (int i = 0; i < word.size(); i++)
+            {   
+                // Now, replace each character of ‘word’ with char
+                // from a-z then check if ‘word’ exists in wordList.
+                char original = word[i];
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    word[i] = c;
+                    if (st.count(word) > 0)
+                    { 
+                        // Check if the word is present in the wordList and
+                        // push the word along with the new sequence in the queue.
+                        vec.push_back(word);
+                        q.push(vec);
+                        // mark as visited on the level
+                        usedOnLevel.push_back(word);
+                        vec.pop_back();
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+bool comp(vector<string> a, vector<string> b)
+{
+    string x = "", y = "";
+    for(string i: a)
+        x += i;
+    for(string i: b)
+        y += i;
+    
+    return x<y;
+}
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<string>wordList(n);
+		for(int i = 0; i < n; i++)cin >> wordList[i];
+		string startWord, targetWord;
+		cin >> startWord >> targetWord;
+		Solution obj;
+		vector<vector<string>> ans = obj.findSequences(startWord, targetWord, wordList);
+		if(ans.size()==0)
+		    cout<<-1<<endl;
+		else
+		{
+		    sort(ans.begin(), ans.end(), comp);
+            for(int i=0; i<ans.size(); i++)
+            {
+                for(int j=0; j<ans[i].size(); j++)
+                {
+                    cout<<ans[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
